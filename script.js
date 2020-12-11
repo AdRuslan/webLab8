19f8c64152642a0aa12ebc0a02d50c5026ba0067
AdRuslan <russian.adig@mail.ru>
2020-12-12 03:47:45 +0500

    task 2

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ for (let i = 1; i < sheepCount + 1; i++) {
   sumStr += `${i} овечка...`;
 }
 
-console.log(sumStr)
+console.log(sumStr);
+
+
+//Task 2
+for (let i = 0; i < 16; i++) {
+    console.log(`${i} ${i % 2 == 0 ? 'четное' : 'нечетное'}`)
+}
