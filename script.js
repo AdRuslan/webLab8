f3364788bc051f370059a809e915fc75e62e1345
AdRuslan <russian.adig@mail.ru>
2020-12-12 04:26:19 +0500

    task 4

--- script.js
+++ script.js
@@ -9,7 +9,55 @@ for (let i = 1; i < sheepCount + 1; i++) {
 console.log(sumStr);
 
 
+
 //Task 2
 for (let i = 0; i < 16; i++) {
-    console.log(`${i} ${i % 2 == 0 ? 'четное' : 'нечетное'}`)
+  console.log(`${i} ${i % 2 == 0 ? "четное" : "нечетное"}`);
+}
+
+
+
+
+//Task 4
+let num1 = 74.7,
+  num2 = 100,
+  num3 = -40;
+
+//сортировка пузырьком
+let bubbleSort = [];
+bubbleSort.push(num1, num2, num3);
+console.log(bubbleSort);
+
+for (let i = 0; i < bubbleSort.length - 1; i++) {
+  for (let j = 0; j < bubbleSort.length - 1; j++) {
+    if (bubbleSort[j] > bubbleSort[j + 1]) {
+      let tmp;
+      tmp = bubbleSort[j];
+      bubbleSort[j] = bubbleSort[j + 1];
+      bubbleSort[j + 1] = tmp;
+    }
+  }
+}
+
+console.log(bubbleSort);
+
+//сортировка if-ми
+if (num1 > num2 && num1 > num3) {
+  if (num2 > num3) {
+    console.log(`${num3}, ${num2}, ${num1}`);
+  } else {
+    console.log(`${num2}, ${num3}, ${num1}`);
+  }
+} else if (num2 > num1 && num2 > num3) {
+  if (num1 > num3) {
+    console.log(`${num3}, ${num1}, ${num2}`);
+  } else {
+    console.log(`${num1}, ${num3}, ${num2}`);
+  }
+} else {
+  if (num2 > num1) {
+    console.log(`${num1}, ${num2}, ${num3}`);
+  } else {
+    console.log(`${num2}, ${num1}, ${num3}`);
+  }
 }
